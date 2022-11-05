13c3175f7598b4f4468160d214ad4862ea73f5ac
SLea <sq2958094330@126.com>
2022-11-05 13:48:51 +0800

    版本1

---
+++ main.c
@@ -0,0 +1,23 @@
+#include "delay.h"
+#include "sys.h"
+#include "usart.h"
+
+
+int main(void)
+ {		
+	u16 times=0;
+	delay_init();	    	 //延时函数初始化	  
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2); //设置NVIC中断分组2:2位抢占优先级，2位响应优先级
+	uart_init(9600);	 //串口初始化为115200
+ 	while(1)
+	{
+//			times++;
+//			if(times%5000==0)
+//			{
+//				printf("TEST+OK\r\n");
+//			}
+		printf("TEST+OK\r\n");
+		delay_ms(500);
+	}	 
+ }
+
